8489b1ba916122f3d91d023183cfde320bc05661
danielabanto <38707113+danielabanto@users.noreply.github.com>
2023-04-25 14:19:04 -0500

    Default port updated

--- models/server.js
+++ models/server.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 class Server {
   constructor() {
     this.app = express();
-    this.port = process.env.PORT;
+    this.port = process.env.PORT || 3002;
     this.usuariosPath = "/api/usuarios";
 
     //Middleware
